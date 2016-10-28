dd1054989df0f727718a7c30b658e7e3d23ac80d
Agoniter <hallvardhh@hotmail.com>
2016-10-28 12:45:35 +0200

    User and message

---
+++ MorseChatServer/src/main/java/Message.java
@@ -0,0 +1,19 @@
+
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hallv
+ */
+@XmlRootElement
+@XmlAccessorType(XmlAccssType.FIELD)
+public class Message {
+    
+}
