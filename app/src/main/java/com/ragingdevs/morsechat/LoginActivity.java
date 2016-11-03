299fd83be4a3bbbb17540ab36859551da5ede397
alexfylling <alexfylling@gmail.com>
2016-11-03 11:55:42 +0100

    Adding login screen

---
+++ app/src/main/java/com/ragingdevs/morsechat/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.ragingdevs.morsechat;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private AutoCompleteTextView mEmail;
+    private EditText mPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
