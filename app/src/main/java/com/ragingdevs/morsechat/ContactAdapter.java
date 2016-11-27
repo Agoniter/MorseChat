5687d721d91a885193e58a1baefe3d2290d8c163
alexfylling <alexfylling@gmail.com>
2016-11-27 18:41:55 +0100

    Added some adapters

---
+++ app/src/main/java/com/ragingdevs/morsechat/ContactAdapter.java
@@ -0,0 +1,38 @@
+package com.ragingdevs.morsechat;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by alexfylling on 27.11.2016.
+ */
+
+public class ContactAdapter extends ArrayAdapter<Contact> {
+
+    public ContactAdapter(Context context, ArrayList<Contact> contacts) {
+        super(context, 0, contacts);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        Contact contact = getItem(position);
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.contact, parent, false);
+        }
+
+        TextView username = (TextView) convertView.findViewById(R.id.username);
+        username.setText(contact.getUsername());
+
+        return convertView;
+    }
+}
